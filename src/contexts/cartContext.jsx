f1998ff93479ffb0362c8e43b3cda44bd50820d3
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2022-08-23 19:06:20 +0300

    added totalitems

--- src/contexts/cartContext.jsx
+++ src/contexts/cartContext.jsx
@@ -9,15 +9,36 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [numberOfItems, setNumberOfItems] = useState(0);
 
-  const addCartItem = (item) => {
-    const listOfItems = cartItems;
-    listOfItems.push(item);
-    setCartItems(listOfItems);
-    setNumberOfItems(listOfItems.length);
+  const addCartItem = (newItem) => {
+    setCartItems((currentItemsInCart) => {
+      if (
+        currentItemsInCart.find((item) => item.name === newItem.name) == null
+      ) {
+        return [...currentItemsInCart, { ...newItem, quantity: 1 }];
+      } else {
+        return currentItemsInCart.map((item) => {
+          if (item.name === newItem.name) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+
+    setTotalItems();
+  };
+
+  const setTotalItems = () => {
+    let numberOftemsInCart = 0;
+    cartItems.forEach((item) => {
+      numberOftemsInCart += item.quantity;
+    });
+    setNumberOfItems(numberOftemsInCart);
   };
 
   useEffect(() => {
-    setNumberOfItems(cartItems.length);
+    setTotalItems();
   }, [cartItems]);
 
   return (
